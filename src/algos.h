675533742e9f3656825ca1edeb199d0bc60e48b7
Xerof <fercho27@gmail.com>
2016-04-25 22:28:53 -0700

    Implemented MergeSort
    Implemented MergeSort and command line
    for the main.cpp, if you want to use MergeSort:
        ./algo merge <file.txt>
    or BubbleSort
        ./algo bubble <file.txt>
    MergeSort is selected by default if there aren't
    command line arguments:
        ./algo  <file.txt>
    
    Signed-off-by: Xerof <fercho27@gmail.com>

--- src/algos.h
+++ src/algos.h
@@ -4,7 +4,7 @@
 #include <vector>
 #include <iterator>
 #include <iostream>
-
+#include <queue>
 
 namespace algos {
 
@@ -18,35 +18,35 @@ namespace algos {
       applySort(first, last);
     }
 
+   template<typename Iterator>
+       void show (Iterator first, Iterator last) {
+        std::cout << "BubbleSort::"<< __func__ << std::endl;
+        for(; first!=last; first++) {
+            std::cout << *first
+                    << " "
+                    << std::endl;
+         }
+       }
+
   protected:
 
     template<typename Iterator>
       void applySort(Iterator first,
                      Iterator last) {
-        std::cout << "BubbleSort: "<< __func__ << std::endl;
+        std::cout << "BubbleSort::"<< __func__ << std::endl;
         // Here goes the code for merge sort declare as manym methods as you want
         // but the interdace of the object has to be only the sort method
 
         Iterator i,j;
         for(i = first ;  i != last; i++) {
-          std::cout<< *i << std::endl;
           for(j = first; j < i; j++) {
             if(*i < *j) {
               swap(i, j);
             }
           }
         }
-        // For now the function will only be printing the data
-        // This is just to show you the way the iterator works
-        Iterator it = first;
-        std::cout << "Printing" << std::endl;
-        for(; it!=last; ++it) {
-          std::cout << *it
-                    << " "
-                    << std::endl;
-        }
+     }
 
-      }
     template<typename Iterator>
         void swap(Iterator it1, Iterator it2) {
             typename std::iterator_traits<Iterator>
@@ -67,21 +67,62 @@ namespace algos {
       applySort(first, last);
     }
 
+   template<typename Iterator>
+       void show (Iterator first, Iterator last) {
+        for(; first!=last; first++) {
+            std::cout << *first
+                    << " "
+                    << std::endl;
+         }
+       }
   protected:
   template<typename Iterator>
     void applySort(Iterator first,
                    Iterator last) {
-      // For now the function will only be printing the data
-      // This is just to show you the way the iterator works
-      std::cout << "Printing" << std::endl;
-      for(; first!=last; first++) {
-        std::cout << *first
-                  << " "
-                  << std::endl;
-      }
-      // Here goes the code for merge sort declare as manym methods as you want
-      // but the interdace of the object has to be only the sort method
+        mergeSort(first, last-1);
     }
+
+    template<typename Iterator>
+        void mergeSort(Iterator first,
+                      Iterator last) {
+
+            if (first < last) {
+                int mid  = (last - first) / 2;
+                mergeSort(first, first + mid);
+                mergeSort(first + mid + 1 , last);
+                merge(first, first + mid, last);
+            }
+
+    }
+
+    template<typename Iterator>
+        void merge(Iterator first,
+                Iterator mid,
+                Iterator last) {
+
+            int i = 0;
+            std::queue<typename std::iterator_traits<Iterator>::value_type> buffer1;
+            std::queue<typename std::iterator_traits<Iterator>::value_type> buffer2;
+
+            for (Iterator i = first; i <= mid; i++)
+                buffer1.push(*i);
+            for (Iterator i = mid +1 ; i <= last; i++)
+                buffer2.push(*i);
+
+
+            while( !buffer1.empty() && !buffer2.empty()) {
+                if(buffer1.front() <= buffer2.front()) {
+                    first[i++] = buffer1.front();
+                    buffer1.pop();
+                } else {
+                    first[i++] = buffer2.front();
+                    buffer2.pop();
+                }
+            }
+            while(!(buffer1.empty())){ first[i++] = buffer1.front(); buffer1.pop();}
+            while(!(buffer2.empty())){ first[i++] = buffer2.front(); buffer2.pop();}
+        }
+
   };
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,36 @@
 #include "algos.h"
 #include <cstdlib>
 #include <fstream>
+#include <string.h>
 
 std::vector<float> readVectorFromFile(const char* fileName);
 
 
 int main(int argc, char** argv) {
 
-  std::vector<float> myData = readVectorFromFile(argv[1]);
+  std::vector<float> myData;
   // Fill the data
+  if(argc != 2) {
+    myData = readVectorFromFile(argv[2]);
+        if (0 == strncmp(argv[1],"merge",5)) {
+            algos::MergeSort al;
+            al.sort(myData.begin(), myData.end());
+            al.show(myData.begin(), myData.end());
+        } else {
+            algos::BubbleSort al;
+            al.sort(myData.begin(), myData.end());
+            al.show(myData.begin(), myData.end());
+        }
+    } else {
+    algos::MergeSort al;
+    myData = readVectorFromFile(argv[1]);
 
-  algos::BubbleSort ms;
+    al.sort(myData.begin(), myData.end());
+    al.show(myData.begin(), myData.end());
 
-  ms.sort(myData.begin(), myData.end());
+    }
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
 std::vector<float> readVectorFromFile(const char* fileName) {
