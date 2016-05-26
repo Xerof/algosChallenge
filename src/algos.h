7399ede51379c971b38c3f7d4a708e3500a154ae
Xerof <fercho27@gmail.com>
2016-05-25 22:29:58 -0500

    Created Class Insertion Sort
    Added the insertion sort algorithm.
    
    Signed-off-by: Xerof <fercho27@gmail.com>

--- src/algos.h
+++ src/algos.h
@@ -22,8 +22,7 @@ namespace algos {
        void show (Iterator first, Iterator last) {
         for(; first!=last; first++) {
             std::cout << *first
-                    << " "
-                    << std::endl;
+                    << " ";
          }
        }
 
@@ -69,8 +68,7 @@ namespace algos {
        void show (Iterator first, Iterator last) {
         for(; first!=last; first++) {
             std::cout << *first
-                    << " "
-                    << std::endl;
+                    << " ";
          }
        }
   protected:
@@ -121,11 +119,46 @@ namespace algos {
             while(!(buffer2.empty())){ first[i++] = buffer2.front(); buffer2.pop();}
         }
 
-  };
+    };
 
+    class InsertionSort {
+      public:
+
+       template<typename Iterator>
+        void sort(Iterator first,
+                  Iterator last) {
+          applySort(first, last);
+        }
+
+       template<typename Iterator>
+           void show (Iterator first, Iterator last) {
+            for(; first!=last; first++) {
+                std::cout << *first
+                        << " ";
+             }
+           }
+      protected:
+      template<typename Iterator>
+        void applySort(Iterator first,
+                       Iterator last) {
+
+            std::cout << "Apply "<< std::endl;
+            Iterator i;
+            Iterator j;
+            for(j = first+1; j < last; ++j) {
+                typename std::iterator_traits<Iterator>
+                    ::value_type key = *j;
+                i = j-1;
+                while(i >= first && *i > key)
+                {
+                    *(i+1) = *i;
+                    i--;
+                    *(i+1) = key;
+                }
+
+            }
+        }
+    };
 
 }
-
-
-
 #endif
